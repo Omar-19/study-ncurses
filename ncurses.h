57e42d8de7318cd5ad6727240b14142568aa7c50
Ferny Yuko <fyuko@oa-n3.21-school.ru>
2020-01-19 17:39:58 +0300

    visual

--- ncurses.h
+++ ncurses.h
@@ -7,18 +7,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <string.h>
 
 typedef struct		s_game_param
 {
-	char	lim[23]; //"Cycles/second limit : \0"
+	char	lim[24]; //"Cycles/second limit : \0"
 	int		ilim;
-	char	cycle[6];
+	char	cycle[9];
 	int		icycle;
 	char	processes[13]; //"Processes : \0"
 	int		iprocesses;
 	char	c_t_d[17];//CYCLES_TO_DIE : \0
 	int		ic_t_d;
-	char	live[5];
+	char	live[8];
 	int		ilive;
 	char	c_delta[15];//CYCLE_DELTA : \O
 	int		ic_delta;
@@ -28,12 +29,12 @@ typedef struct		s_game_param
 
 typedef struct		s_players_param
 {
-	char	player[10];//Player - \0}
+	char	player[10];//Player - \0
 	int		num_player;
 	char	*player_name;
 	char	last_live[13];//Last live : \0
 	int		ilast_live;
-	char	l_in_current_p[26];//Lives in current period : \0
+	char	l_in_current_p[27];//Lives in current period : \0
 	int		il_in_current_p;
 	int		color;
 }					t_players_param;
