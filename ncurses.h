3253c049bedda15a1ef5fe98f96bc713774c3df9
Ferny Yuko <fyuko@oa-n3.21-school.ru>
2020-01-19 16:10:40 +0300

    header updated

--- ncurses.h
+++ ncurses.h
@@ -16,11 +16,26 @@ typedef struct		s_game_param
 	int		icycle;
 	char	processes[13]; //"Processes : \0"
 	int		iprocesses;
+	char	c_t_d[17];//CYCLES_TO_DIE : \0
+	int		ic_t_d;
+	char	live[5];
+	int		ilive;
+	char	c_delta[15];//CYCLE_DELTA : \O
+	int		ic_delta;
+	char	nbr_l_max_checks[23];//NBR_LIVE MAX_CHECKS : \0
+	int		inbr_l_max_checks;
 }					t_game_param;
 
 typedef struct		s_players_param
 {
-
+	char	player[10];//Player - \0}
+	int		num_player;
+	char	*player_name;
+	char	last_live[13];//Last live : \0
+	int		ilast_live;
+	char	l_in_current_p[26];//Lives in current period : \0
+	int		il_in_current_p;
+	int		color;
 }					t_players_param;
 
 #endif
